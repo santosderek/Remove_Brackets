40cc347cc4f2e140f387b8e09d41a0039a90b0a5
Derek Santos <derekhalo@gmail.com>
2017-11-07 21:43:25 -0500

    Initial Commit

---
+++ remove_brackets/main.py
@@ -0,0 +1,41 @@
+from os import listdir, rename, chdir
+from os.path import isfile, join
+
+import re
+
+DIRECTORY_TO_CHANGE = r'Z:\plex_storage\Anime Shows Additional\Kono Bijutsubu ni wa Mondai ga Aru! [TV][720p][ArtClub]'
+chdir(DIRECTORY_TO_CHANGE)
+
+def remove_brackets(filename):
+    return re.sub("([\(\[]).*?([\)\]])", "", filename)
+
+def get_list_of_files(directory):
+    return [filename for filename in listdir(directory) if isfile(join(directory, filename))]
+
+def rename_files(old_list: list, new_list: list):
+    for old_file, new_file in zip(old_list, new_list):
+        try:
+            if old_file == new_file:
+                print ('Skipping:', old_file)
+            else:
+                print ('Old:', old_file, ', New', new_file)
+                rename(old_file, new_file)
+        except Exception as e:
+            print ('ERROR:', e)
+
+def main():
+    files_with_brackets = get_list_of_files(DIRECTORY_TO_CHANGE)
+    #print ('Before:', files_with_brackets)
+
+    files_without_brackets = [remove_brackets(singlefile) for singlefile in files_with_brackets]
+    #print ('After:', files_without_brackets)
+
+    print ('----Starting to rename ----')
+    rename_files(files_with_brackets, files_without_brackets)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
